f5fbaba0d88c2c477e89817174fe3d2b5d40c5d0
givanz <givanz@yahoo.com>
2018-12-14 17:45:29 +0200

    Fixed css unit input empty unit text if empty select is not changed first. Added ajax save option and sample php code.

---
+++ save.php
@@ -0,0 +1,11 @@
+<?php
+define('MAX_FILE_LIMIT', 1024 * 1024 * 2);//2 Megabytes max html file size
+
+//sanitize and remove double dot ..
+$fileName = __DIR__ . '/' . preg_replace('@\.{2,}@' , '', preg_replace('@[^\/a-zA-Z0-9\-\._]@','', $_POST['fileName']));
+
+$html = substr($_POST['html'], 0, MAX_FILE_LIMIT);
+
+if (file_put_contents($fileName, $html)) 
+echo $fileName;
+else echo 'Error saving file '  . $fileName;
