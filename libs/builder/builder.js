75134cfa36799e98ea8f5c88e44b28b6363ec394
stevenge1996 <steven-ge@hotmail.com>
2020-02-08 00:42:37 +0900

    Modified the argument for addPage function call to fix the inability to add new pages

--- libs/builder/builder.js
+++ libs/builder/builder.js
@@ -1593,7 +1593,7 @@ Vvveb.Gui = {
 			var url = "" + fileName;
 			
 
-			Vvveb.FileManager.addPage(name, title, url);
+			Vvveb.FileManager.addPage(name, {title: url, url: url});
 			event.preventDefault();
 
 			return Vvveb.Builder.saveAjax(url, startTemplateUrl, function (data) {
