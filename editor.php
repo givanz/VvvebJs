21b426394ec90127f358f6b61384f4754a560270
givanz <givanz@yahoo.com>
2024-04-29 14:04:16 +0300

    Removed GLOB_BRACE from glob for compatibility with OSes that lacks support like Alpine Linux

--- editor.php
+++ editor.php
@@ -3,7 +3,8 @@
 $html = file_get_contents('editor.html');
 
 //search for html files in demo and my-pages folders
-$htmlFiles = glob('{my-pages/*.html,demo/*\/*.html, demo/*.html}',  GLOB_BRACE);
+//$htmlFiles = glob('{my-pages/*.html,demo/*\/*.html, demo/*.html}',  GLOB_BRACE);
+$htmlFiles = array_merge(glob('my-pages/*.html'),glob('demo/*\/*.html'), glob('demo/*.html'));
 $files = '';
 foreach ($htmlFiles as $file) { 
    if (in_array($file, array('new-page-blank-template.html', 'editor.html'))) continue;//skip template files
