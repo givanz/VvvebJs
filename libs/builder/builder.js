3b58018768e1cd4a126fe902f73ec823a1379dd8
hayden-t <hayden-t@users.noreply.github.com>
2025-10-15 14:54:16 +1100

    variable spelling mistake

--- libs/builder/builder.js
+++ libs/builder/builder.js
@@ -1454,7 +1454,7 @@ Vvveb.Builder = {
 								if (noChildren[parentTagName] || isVattribute) { 
 									parent.after(self.dragElement);
 								} else {
-									if (parent == self.dragElement.parenNode) {
+									if (parent == self.dragElement.parentNode) {
 										parent.appendChild(self.dragElement);
 									} else {
 										parent.append(self.dragElement);
